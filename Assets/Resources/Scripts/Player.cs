33c1fe3673d443e4ac324269b02432fd792967db
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2013-10-05 15:29:23 +0200

    Player

---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    private float angle;
+    CharacterMotor motor;
+	void Start () {
+        motor = GetComponent<CharacterMotor>();
+        if (motor == null)
+            Destroy(this);
+	}
+	
+	void Update () {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+            transform.LookAt(transform.position + new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+            motor.inputMoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        } else {
+            motor.inputMoveDirection = Vector3.zero;
+        }
+        //motor.inputMoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	}
+}
