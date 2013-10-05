94e2b0725004a2921978c20a59b98a5fcb6a79ca
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2013-10-05 14:23:55 +0200

    Moving Islands

--- Assets/Resources/Scripts/Floor.cs
+++ Assets/Resources/Scripts/Floor.cs
@@ -2,74 +2,94 @@
 using System.Collections;
 
 public class Floor : MonoBehaviour {
-    private Stack islands;
+    private Stack removeIsland;
+    private ArrayList islands;
     private Transform[,] transforms;
     private float[,] positions;
+    private float[,] jitter;
     public uint width;
     public uint height;
+    public Transform tile;
 
-    void Start()
-    {
-        GameObject tile = Resources.Load("tile") as GameObject;
+    void Start() {
         if (width == 0) width = 32;
         if (height == 0) height = 32;
         positions = new float[width, height];
         transforms = new Transform[width, height];
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                GameObject g = Instantiate(tile) as GameObject;
-                g.transform.position = new Vector3(transform.position.x + i - width/2, transform.position.y, transform.position.z + j - height/2);
+        removeIsland = new Stack();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                GameObject g = (Instantiate(tile) as Transform).gameObject;
+                g.transform.position = new Vector3(transform.position.x + i - width / 2, transform.position.y, transform.position.z + j - height / 2);
                 g.transform.Rotate(0, 0, Mathf.Floor(Random.value * 4) * 90);
                 transforms[i, j] = g.transform;
-                positions[i, j] = g.transform.position.y - 5 + Mathf.PerlinNoise(g.transform.position.x/2,g.transform.position.z/2);
+                positions[i, j] = transform.position.y - 5 + Mathf.PerlinNoise(g.transform.position.x / 2, g.transform.position.z / 2);
             }
         }
 
-        islands = new Stack();
-        islands.Push(new Island(1,1,31,31));
-        AddIsland(islands.Peek() as Island);
-	}
+        islands = new ArrayList();
 
-    void AddIsland(Island island)
-    {
-        for (int i = island.x; i < Mathf.Min(width,island.x + island.width); i++)
-        {
-            for (int j = island.y; j < Mathf.Min(height, island.y + island.height); j++)
-            {
-                if(island.heights[i - island.x, j - island.y] != 0)
+        AddIsland(new Island(3, 3, 20, 20));
+        AddIsland(new Island(13, 3, 20, 20));
+        AddIsland(new Island(3, 13, 20, 20));
+        AddIsland(new Island(13, 13, 20, 20));
+    }
+
+    void AddIsland(Island island) {
+        islands.Add(island);
+        for (int i = Mathf.Max(island.x,0); i < Mathf.Min(width, island.x + island.width); i++) {
+            for (int j = Mathf.Max(island.y, 0); j < Mathf.Min(height, island.y + island.height); j++) {
+                if (island.heights[i - island.x, j - island.y] != 0)
                     positions[i, j] = island.heights[i - island.x, j - island.y];
             }
         }
     }
+    void RemoveIsland(Island island) {
+        removeIsland.Push(island);
+        for (int i = Mathf.Max(island.x, 0); i < Mathf.Min(width, island.x + island.width); i++) {
+            for (int j = Mathf.Max(island.y, 0); j < Mathf.Min(height, island.y + island.height); j++) {
+                if (island.heights[i - island.x, j - island.y] != 0)
+                    positions[i, j] = transform.position.y - 5 + Mathf.PerlinNoise(transforms[i, j].position.x / 2, transforms[i, j].position.z / 2);
+            }
+        }
+    }
 
-    void Update()
-    {
+    void Update() {
         Transform t;
         float dist;
         float move;
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 t = transforms[i, j];
                 dist = positions[i, j] - t.position.y;
-                move = Mathf.Sign(dist) * Time.deltaTime * Mathf.Min(1,Mathf.Abs(dist));
-                
-                t.position = new Vector3(i, t.position.y + Mathf.Lerp(move,(Random.value-0.5f)*Time.deltaTime,0.3f), j);
+                move = Mathf.Sign(dist) * Time.deltaTime * Mathf.Min(1, Mathf.Abs(dist));
+
+                t.position = new Vector3(i, t.position.y + Mathf.Lerp(move, (Random.value - 0.5f) * Time.deltaTime, 0.3f), j);
             }
         }
-	}
+
+        foreach (Island i in islands) {
+            i.life -= Time.deltaTime;
+            if (i.life <= 0) {
+                RemoveIsland(i);
+            }
+        }
+        while (removeIsland.Count > 0) {
+            islands.Remove(removeIsland.Pop() as Island);
+            AddIsland(new Island(Mathf.FloorToInt(Random.value * 32) - 8, Mathf.FloorToInt(Random.value * 32) - 8, 20, 20));
+            if(islands.Count < 18)
+                AddIsland(new Island(Mathf.FloorToInt(Random.value * 32) - 8, Mathf.FloorToInt(Random.value * 32) - 8, 20, 20));
+        }
+    }
 }
-public class Island
-{
+public class Island {
+    public float life;
     public int x, y;
     public int width, height;
     public float[,] heights;
 
-    public Island(int x, int y, int width, int height)
-    {
+    public Island(int x, int y, int width, int height) {
+        this.life = Random.value * 15 + 5;
         this.x = x;
         this.y = y;
         this.width = width;
@@ -79,22 +99,16 @@ public class Island
         float sum = 0;
         float[,] random = new float[width, height];
         float[,] blur = new float[width, height];
-        for (uint i = 1; i < width - 1; i++)
-        {
-            for (uint j = 1; j < height - 1; j++)
-            {
+        for (uint i = 1; i < width - 1; i++) {
+            for (uint j = 1; j < height - 1; j++) {
                 random[i, j] = Random.value;
             }
         }
-        for (uint i = 1; i < width - 1; i++)
-        {
-            for (uint j = 1; j < height - 1; j++)
-            {
+        for (uint i = 1; i < width - 1; i++) {
+            for (uint j = 1; j < height - 1; j++) {
                 sum = 0;
-                for (int k = -1; k <= 1; k++)
-                {
-                    for (int l = -1; l <= 1; l++)
-                    {
+                for (int k = -1; k <= 1; k++) {
+                    for (int l = -1; l <= 1; l++) {
                         sum += random[i + k, j + l];
                     }
                 }
@@ -102,25 +116,52 @@ public class Island
             }
         }
 
-        for (uint i = 0; i < width; i++)
-        {
-            for (uint j = 0; j < height; j++)
-            {
-                if (Mathf.Sqrt((width / 2 - i) * (width / 2 - i) + (height / 2 - j) * (height / 2 - j)) < width / 2)
-                {
-                    heights[i, j] = 1;
+        float h;
+        for (uint i = 0; i < width; i++) {
+            for (uint j = 0; j < height; j++) {
+                h = ((width / 2) - Mathf.Sqrt((width / 2 - i) * (width / 2 - i) + (height / 2 - j) * (height / 2 - j))) / (width / 2);
+                if (Mathf.Sqrt((width / 2 - i) * (width / 2 - i) + (height / 2 - j) * (height / 2 - j)) < width / 2) {
+                    heights[i, j] = Mathf.Lerp(blur[i, j], h, h) * 2.5f;
                 }
             }
         }
 
-        float h;
-        for (uint i = 1; i < width - 1; i++)
-        {
-            for (uint j = 1; j < height - 1; j++)
+        int destroy = Mathf.FloorToInt(width/3f);
+        int kills;
+        for (int i = 0; i < width; i++) {
+            kills = Mathf.FloorToInt((Random.value + Random.value) / 2 * destroy) + 1;
+            for (int j = 0; j < height && kills > 0; j++) {
+                if (heights[i, j] > 0) {
+                    heights[i, j] = 0;
+                    kills--;
+                }
+            }
+            kills = Mathf.FloorToInt((Random.value + Random.value)/2 * destroy) + 1;
+            for (int j = height - 1; j >= 0 && kills > 0; j--) {
+                if (heights[i, j] > 0) {
+                    heights[i, j] = 0;
+                    kills--;
+                }
+            }
+        }
+        for (int i = 0; i < height; i++) {
+            kills = Mathf.FloorToInt((Random.value + Random.value) / 2 * destroy) + 1;
+            for (int j = 0; j < width && kills > 0; j++)
             {
-                h = ((width / 2) - Mathf.Sqrt((width / 2 - i) * (width / 2 - i) + (height / 2 - j) * (height / 2 - j))) / (width / 2);
-                if(heights[i,j] != 0)
-                    heights[i,j] = Mathf.Lerp(blur[i, j],h,h)*3;
+                if (heights[j, i] > 0)
+                {
+                    heights[j, i] = 0;
+                    kills--;
+                }
+            }
+            kills = Mathf.FloorToInt((Random.value + Random.value) / 2 * destroy) + 1;
+            for (int j = width - 1; j >= 0 && kills > 0; j--)
+            {
+                if (heights[j, i] > 0)
+                {
+                    heights[j, i] = 0;
+                    kills--;
+                }
             }
         }
     }
