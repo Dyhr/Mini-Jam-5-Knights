89f19783fdceb2fb6e8595b66c3050f0a8210448
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2013-10-05 02:36:27 +0200

    First Platform

--- Assets/Floor.cs
+++ Assets/Floor.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class Floor : MonoBehaviour {
+    private Stack islands;
     private Transform[,] transforms;
     private float[,] positions;
     public uint width;
@@ -21,10 +22,26 @@ public class Floor : MonoBehaviour {
                 GameObject g = Instantiate(tile) as GameObject;
                 g.transform.position = new Vector3(transform.position.x + i - width/2, transform.position.y, transform.position.z + j - height/2);
                 transforms[i, j] = g.transform;
-                positions[i, j] = transform.position.y - 5 + Random.value;
+                positions[i, j] = g.transform.position.y - 5 + Mathf.PerlinNoise(g.transform.position.x/2,g.transform.position.z/2);
             }
         }
+
+        islands = new Stack();
+        islands.Push(new Island(1,1,31,31));
+        AddIsland(islands.Peek() as Island);
 	}
+
+    void AddIsland(Island island)
+    {
+        for (int i = island.x; i < Mathf.Min(width,island.x + island.width); i++)
+        {
+            for (int j = island.y; j < Mathf.Min(height, island.y + island.height); j++)
+            {
+                if(island.heights[i - island.x, j - island.y] != 0)
+                    positions[i, j] = island.heights[i - island.x, j - island.y];
+            }
+        }
+    }
 
     void Update()
     {
@@ -41,4 +58,55 @@ public class Floor : MonoBehaviour {
             }
         }
 	}
+}
+public class Island
+{
+    public int x, y;
+    public int width, height;
+    public float[,] heights;
+
+    public Island(int x, int y, int width, int height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        heights = new float[width, height];
+
+        for (uint i = 0; i < width; i++)
+        {
+            for (uint j = 0; j < height; j++)
+            {
+                if (Mathf.Sqrt((width / 2 - i) * (width / 2 - i) + (height / 2 - j) * (height / 2 - j)) < width / 2)
+                {
+                    heights[i, j] = ((width / 2) - Mathf.Sqrt((width / 2 - i) * (width / 2 - i) + (height / 2 - j) * (height / 2 - j))) / (width / 2) * Random.value * 2 + 2;
+                }
+            }
+        }
+        float sum = 0;
+        float[,] blur = new float[width, height];
+        for (uint i = 1; i < width-1; i++)
+        {
+            for (uint j = 1; j < height-1; j++)
+            {
+                sum = 0;
+                for (int k = -1; k <= 1; k++)
+                {
+                    for (int l = -1; l <= 1; l++)
+                    {
+                        sum += heights[i + k, j + l];
+                    }
+                }
+                blur[i, j] = sum/9;
+            }
+        }
+        for (uint i = 1; i < width - 1; i++)
+        {
+            for (uint j = 1; j < height - 1; j++)
+            {
+                if(heights[i,j] != 0)
+                    heights[i,j] = blur[i, j];
+            }
+        }
+    }
 }
