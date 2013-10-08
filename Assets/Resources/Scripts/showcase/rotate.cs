379810b28b0f06cabc19b28079e2a393275f3b16
Frans Peter Larsen <larsen.frans@gmail.com>
2013-10-08 12:22:25 +0200

    Rotating Object in Showcase

---
+++ Assets/Resources/Scripts/showcase/rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+	
+	public float speed = 10f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+	}
+}
